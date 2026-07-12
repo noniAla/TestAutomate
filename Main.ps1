501f162ae230f5a1d4a1c2bf4d3f0ca9425f7b56
noniAla <noora56457@gmail.com>
2026-07-12 14:11:07 +0300

    Update Main.ps1 with validation included

--- Main.ps1
+++ Main.ps1
@@ -1,6 +1,15 @@
 Import-Module .\Modules\Graph.psm1
+Import-Module .\Modules\Validation.psm1
 
 Write-Host "Main.ps1 started"
+
+$request = @{
+    ResourceType = "Microsoft365Group"
+    Name= "HR Group"
+    Owner = "owner@company.com"
+}
+
+Test-RequestPayload -Request $request
 
 Connect-GraphService
 
