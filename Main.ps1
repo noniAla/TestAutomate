3b12c06accc447b5f1edfc30989e04009c45a85b
noniAla <noora56457@gmail.com>
2026-07-12 14:54:04 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -4,9 +4,16 @@ Import-Module .\Modules\Validation.psm1
 Write-Host "Main.ps1 started"
 
 $request = @{
-    ResourceType = "Microsoft365Group"
-    Name= "HR Group"
-    Owner = "owner@company.com"
+    ResourceType = "Microsoft365Group",
+    Name= "HR Group",
+    Description= "This group's purpose is to.....",
+    Ownermail = "owner@company.com",
+    Memebers = [
+    "user1@company.com",
+    "user2@company.com",
+    "user3@company.com"
+    ],
+    GroupType= "Microsoft365Group"
 }
 
 Test-RequestPayload -Request $request
