98532b526aa29ca2c2b824d0a4f74d712b3e1007
noniAla <noora56457@gmail.com>
2026-07-12 14:07:49 +0300

    Update Validation.psm1

--- Modules/Validation.psm1
+++ Modules/Validation.psm1
@@ -1,1 +1,37 @@
+function Test-RequestPayload {
+  [CmdletBinding()]
+  param(
+      [Parameter(Madatory)]
+      $Request
+  )
+  Write-Host "Starting validation.."
+  
+  $ValidateResourceTypes = @(
+  "Team",
+  "Microsoft365Group",
+  "SecurityGroup",
+  "DistributionList",
+  "MailEnabledSecurityGroup",
+  "SharePoint"
+  )
 
+  if($Request.ResourceType -notin $ValidateResourceTypes) {
+    throw "Invalid Resource Type: $(Request.ResourceType)"
+    }
+
+  if (:IsNullORWhiteSpace($Request.Name))  {
+      throw "Name is Required."
+      }
+
+  if (:IsNullORWhiteSpace($Request.Owner))  {
+      throw "Owner is Required."
+      }
+
+  Write-Host "Validation successful."
+
+  return $true
+
+}
+
+Export-ModuleMember -Function *
+  
