1babc5c45953d81ab57d4ef647ce0fbdeb01fd38
halqaed <hussain.n.k07@gmail.com>
2026-07-12 13:30:20 +0300

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,1 +1,14 @@
+Import-Module .\Modules\Graph.psm1
 
+Write-Host "Main.ps1 started"
+
+Connect-GraphService
+
+Get-GraphContextInfo
+
+if (Test-GraphConnection) {
+    Write-Host "Graph test passed"
+}
+else {
+    Write-Host "Graph test failed"
+}
