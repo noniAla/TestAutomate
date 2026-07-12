ee8a3b9b196e8da1267b7e8a5288bbd68950fecc
noniAla <noora56457@gmail.com>
2026-07-12 16:03:23 +0300

    Update Exchange.psm1

--- Modules/Exchange.psm1
+++ Modules/Exchange.psm1
@@ -1,1 +1,51 @@
 
+# Connect to ms sign in page and create a session
+function Connect-ExchangeService {
+  [CmdletBinding()]
+  paran()
+  
+  Write-Host "Connecting to Exchange Online..."
+  Connect-ExchangeOnline
+  
+  Write-Host "Connected."
+}
+
+# test if that session is reachable (returns true/false)
+function Test-ExchangeConnection {
+[cmdleyBinding()]
+param()
+
+try {
+  Get-AcceptedDomain -ErrorAction Stop | Out-Null 
+  return $true
+}
+catch {
+  return $false
+  }
+}
+
+# Just to output if the session is active
+function GetExchangeContextInfo {
+[cmdletBonding()]
+param
+Write-Host "Exchange Online session Active."
+}
+
+
+# Initial group creation code (Incomplete + will be expanded tommorow)
+function New-DistributionList {
+param([string]$Name)
+
+Write-Host "Creating Distribution List: $Name"
+}
+
+
+# Initial group creation code (Incomplete + will be expanded tommorow)
+function new-MailEnabledSecurityGroup{
+param([string]$Name)
+Write-Host "Creating a Mail-Enabled Group: $Name"
+}
+
+
+Export-ModuleMember -Function *
+
