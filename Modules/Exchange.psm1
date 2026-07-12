bc07d6c026d61db842086bbd2cd2a03c0a75e6db
noniAla <noora56457@gmail.com>
2026-07-12 16:33:24 +0300

    Update Exchange.psm1 fixed some typos

--- Modules/Exchange.psm1
+++ Modules/Exchange.psm1
@@ -2,7 +2,7 @@
 # Connect to ms sign in page and create a session
 function Connect-ExchangeService {
   [CmdletBinding()]
-  paran()
+  param()
   
   Write-Host "Connecting to Exchange Online..."
   Connect-ExchangeOnline
@@ -12,7 +12,7 @@ function Connect-ExchangeService {
 
 # test if that session is reachable (returns true/false)
 function Test-ExchangeConnection {
-[cmdleyBinding()]
+[cmdletBinding()]
 param()
 
 try {
@@ -25,9 +25,9 @@ catch {
 }
 
 # Just to output if the session is active
-function GetExchangeContextInfo {
-[cmdletBonding()]
-param
+function Get-ExchangeContextInfo {
+[cmdletBinding()]
+param([string]$Name)
 Write-Host "Exchange Online session Active."
 }
 
@@ -41,7 +41,7 @@ Write-Host "Creating Distribution List: $Name"
 
 
 # Initial group creation code (Incomplete + will be expanded tommorow)
-function new-MailEnabledSecurityGroup{
+function New-MailEnabledSecurityGroup{
 param([string]$Name)
 Write-Host "Creating a Mail-Enabled Group: $Name"
 }
