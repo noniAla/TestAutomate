61bf88081088215809de178a8bac1f4af0b1c8eb
halqaed <hussain.n.k07@gmail.com>
2026-07-12 13:30:40 +0300

    Update Graph.psm1

--- Modules/Graph.psm1
+++ Modules/Graph.psm1
@@ -1,1 +1,34 @@
+function Connect-GraphService {
+    [CmdletBinding()]
+    param()
 
+    Write-Host "Connecting to Microsoft Graph..."
+
+    Connect-MgGraph
+
+    Write-Host "Connected."
+}
+
+function Get-GraphContextInfo {
+    [CmdletBinding()]
+    param()
+
+    Get-MgContext
+
+}
+
+function Test-GraphConnection {
+    [CmdletBinding()]
+    param()
+
+    try {
+        $user = Get-MgUser -Top 1 -ErrorAction Stop
+
+        return $true
+    }
+    catch {
+        return $false
+    }
+}
+
+Export-ModuleMember -Function *
