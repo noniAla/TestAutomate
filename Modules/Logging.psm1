96c19cd44e66124baeb081f6e365840f0bec08a5
noniAla <noora56457@gmail.com>
2026-07-13 11:08:44 +0300

    Update Logging.psm1

--- Modules/Logging.psm1
+++ Modules/Logging.psm1
@@ -1,1 +1,41 @@
 
+
+# This is the oprational Log
+
+function Write-OprationalLog {
+ [CmdletBinding()]
+ param(
+ [string]$Category,
+ [string]$Message
+ )
+
+ $Timestamp = Get-Date -Formate "yyyy-MM-dd HH:mm:ss"
+ Write-Host ""
+ Write-Host "[$Timestamp][OPERATIONAL]"
+ Write-Host "[$Category]"
+ Write-Host $Message
+}
+
+
+
+
+
+
+# This is the Audit log
+
+function Write-AuditLog {
+    [cmdletBinding()]
+    param(
+        [string]$RequestId,
+        [string]$Event
+        )
+
+     $Timestamp = Get-Date -Formate "yyyy-MM-dd HH:mm:ss"
+     Write-Host ""
+     Write-Host "[$Timestamp][AUDIT]"
+     Write-Host "[$Request]"
+     Write-Host $Event
+
+}
+
+Export-ModuleMember -Function *
