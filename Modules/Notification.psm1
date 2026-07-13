b39cc4e8b31c04fcea8bdaa43cda0c9f7edecca7
noniAla <noora56457@gmail.com>
2026-07-13 11:09:36 +0300

    Update Notification.psm1

--- Modules/Notification.psm1
+++ Modules/Notification.psm1
@@ -1,1 +1,18 @@
+function Send-Notification {
+    [CmdletBinding()]
+    param(
+        [string]$Reciepent,
+        [string]$Subject,
+        [string]$Message
+        )
 
+         Write-Host " "            
+         Write-Host "============== Notification =============="
+         Write-Host "To: $Recipent"
+         Write-Host "Subject: $Subject"
+         Write-Host "Message: $Message"
+         Write-Host "=========================================="
+         Write-Host " "
+
+Export-ModuleMember -Function*
+}
