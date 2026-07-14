a0a70bf00503ebc1f261149a0fee7113deac57ae
noniAla <noora56457@gmail.com>
2026-07-14 12:14:00 +0300

    Update M365GroupProvision.ps1

--- Modules/Provisioning/M365GroupProvision.ps1
+++ Modules/Provisioning/M365GroupProvision.ps1
@@ -1,1 +1,66 @@
+function Invoke-M365GroupProvision {
 
+    [CmdletBinding()]
+    param(
+        [hashtable]$Request
+        )
+
+    # Optional, could be used in the main Azure function
+    Write-AuditLog  `
+        -RequestId "REQ-001"  `
+        -Event "PRovisioning Started"
+
+    # Validate request 
+    Validate-M365GroupRequest `
+        -Request $Request
+
+    #Create Group
+    $GroupId = New-M365Group `
+        -DisplayName $Request.Name `
+        -Description $Request.Description
+    
+    #Resolve Owner (Tempt)
+    $OwnerId = Get-UserIdsByMail `
+        -Mails $Request.OwnerMail
+
+    #Add Owner
+    foreach($OwnerId in $OwnerIds)
+    {
+        Add-GroupOwnership `
+            -GroupId $GroupId
+            -OwnerId $OwnerId
+    }
+    
+
+    #Resolve members
+    $MemeberIds = Get-UserIdsByMail `
+        -Mails $Request.Members
+
+
+    # Add Members 
+    foreach($MemberId in $MemberIds)
+    {
+        Add-GroupMembers `
+            -RequestId "REQ-001"`
+            -Event "Provisioning Completed"
+    }
+
+    Write-AuditLog  `
+        -RequestId "REQ-001"  `
+        -Event "Provisioning Completed"
+
+    # Send Notification to all owners
+    foreach($OwnerId in $OwnerIds) {
+    Send-Notification `
+        -Recipient $Request.OwnerMail `
+        -Subject "Group Created" `
+        -Message "microsoft 365 Group created successfully"
+
+    }
+
+    return $GroupId
+}
+            
+
+
+  
