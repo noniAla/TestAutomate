7f4921c8345eeaa5da0257a43172832381fb82d3
noniAla <noora56457@gmail.com>
2026-07-15 12:06:37 +0300

    Create MailEnabledSecurityGroupValidation.ps1

---
+++ Modules/Validation/MailEnabledSecurityGroupValidation.ps1
@@ -0,0 +1,73 @@
+. "$PSScriptRoot\..\Exchange\MailEnabledSecurityGroups.ps1"
+
+function Validate-MailEnabledSecurityGroupRequest {
+   
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [object]$Request
+        )
+
+
+    # If display name is empty 
+    if (:IsNullOrWhiteSpace($Request.DisplayName))
+    {
+        throw "Display Name is required."
+
+    }
+
+
+    # If Alias is empty 
+    if (:IsNullOrWhiteSpace($Request.Alias))
+    {
+        throw "Alias is required."
+
+    }
+
+
+    # If Primary Smtp Address is empty 
+    if (:IsNullOrWhiteSpace($Request.PrimarySmtpAddress))
+    {
+        throw "Primary Smtp Address is required."
+
+    }
+
+
+     # If no memeber is assigned
+     if ($null -eq $Request.Members -or $Request.Members.Count -eq 0) {
+
+        throw "At least one member is required."
+     }
+
+     
+      # If no owner is assigned
+      if ($null -eq $Request.Owners -or $Request.Owners.Count -eq 0) {
+
+        throw "At least one owner is required."
+     }
+
+
+
+     #Validate SMTP Format:
+     if ($Request.PrimarySmtp -notmatch  '^[^@]+@[^@]+\.[^@]+$')
+     {
+        throw "PRimary SMTP address is invalid."
+     }
+
+
+     # Duplicated Group
+     if (Test-MailEnabledSecurityGroupExists `
+            -DisplayName $Request.DisplayName)
+        {
+            throw "A Mail-Enabled Security Group named '$($Request.DisplayName)' already exists."
+        }
+
+     
+     # Naming Convention
+     if ($Request.DisplayName -notmatch '^MES-')
+     {
+        throw "Mail Enabled Security Group names must start with 'MES-'."
+     } 
+
+
+}
