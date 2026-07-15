cc470160986b3aba5738a4ef1b8db7b78cb725f7
noniAla <noora56457@gmail.com>
2026-07-15 09:47:24 +0300

    Update TeamValidation.ps1

--- Modules/Validation/TeamValidation.ps1
+++ Modules/Validation/TeamValidation.ps1
@@ -1,1 +1,18 @@
+function Validate-TeamRequest {
 
+     [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [object]$Request
+        )
+
+          
+     # Naming Convention
+     if($Request.DisplayName -notmatch '^TEAM-')
+     {
+        throw "Team names must start with 'TEAM-'."
+     }
+
+     # Possible more specific validations for teams in the future
+
+}
